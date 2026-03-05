2a972d80af7bfe872c75ed4bc37bb1745c552af1
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2026-03-05 21:23:47 +0530

    Creates one quiz question and a submit button which just alerts 'hello world'

--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,3 +1,7 @@
 globalThis.onload = () => {
-  console.log("Hello World");
+  const submitBtn = document.querySelector('form button')  
+  submitBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    alert('Hello World')
+  })
 };
